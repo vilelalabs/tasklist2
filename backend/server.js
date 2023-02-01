259622331cdccafa45affc4badda6f808e88db9d
Henrique Vilela <hvilelaeng@gmail.com>
2023-02-01 00:12:51 -0300

    new get query, responses translate.

--- backend/server.js
+++ backend/server.js
@@ -33,44 +33,54 @@ const client = new Client({
 client.connect();
 
 app.get('/data', (req, res) => {
-    client.query('SELECT * FROM tasks ORDER BY id ASC', (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Error reading DB');
-        }
-        if (result.rows.length === 0) {
-            res.status(204).send('No tasks found');
-            return  
-        }
-        res.status(200).send(result.rows);
-    });
+    client.query(`SELECT * FROM tasks
+                    ORDER BY
+                    CASE WHEN status != 'stopped' THEN 1 ELSE 2 END,
+                    id ASC;`,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Erro interno do servidor');
+            }
+            if (result.rows.length === 0) {
+                res.status(204).send('Nenhuma tarefa encontrada.');
+                return
+            }
+            res.status(200).send(result.rows);
+        });
 });
 
 app.post('/data', (req, res) => {
     const { name } = req.body;
     const status = 'unitialized';
+
+    if (!name) {
+        res.status(400).send('Nome da tarefa é obrigatório.');
+        return;
+    }
+
     client.query(`INSERT INTO tasks (name, status) VALUES ($1,$2)`, [name, status], (err, result) => {
         if (err) {
             console.log(err);
-            res.status(500).send('Error inserting new task');
+            res.status(500).send('Erro interno do Servidor.');
         }
-        res.status(201).send('Task created');
+        res.status(201).send('Tarefa Criada.');
     });
 });
 
 app.put('/data', (req, res) => {
     const { id, status, updated_at } = req.body;
     if (status !== 'playing' && status !== 'paused' && status !== 'stopped' && status !== 'unitialized') {
-        res.status(400).send('Invalid status');
+        res.status(400).send('Status Inválido.');
     }
     // find id in database
     client.query(`SELECT * FROM tasks WHERE id = $1`, [id], (err, result) => {
         if (err) {
             console.log(err);
-            res.status(500).send('Error finding task');
+            res.status(500).send('Erro interno do servidor.');
         }
         if (result.rows.length === 0) {
-            res.status(404).send('Task not found');
+            res.status(404).send('Tarefa não encontrada.');
         }
     });
 
@@ -79,9 +89,9 @@ app.put('/data', (req, res) => {
     client.query(`UPDATE tasks SET status = $1, updated_at = $2, finished_at = $3 WHERE id = $4`, [status, updated_at, finished_at, id], (err, result) => {
         if (err) {
             console.log(err);
-            res.status(500).send('Error updating task');
+            res.status(500).send('Erro interno do servidor.');
         }
-        res.status(200).send('Task updated');
+        res.status(200).send('Status atualizado.');
     });
 });
 
@@ -92,19 +102,19 @@ app.put('/data/updatetimer', (req, res) => {
     client.query(`SELECT * FROM tasks WHERE id = $1`, [id], (err, result) => {
         if (err) {
             console.log(err);
-            res.status(500).send('Error finding task');
+            res.status(500).send('Erro interno do servidor.');
         }
         if (result.rows.length === 0) {
-            res.status(404).send('Task not found');
+            res.status(404).send('Tarefa não encontrada.');
         }
     });
 
     client.query(`UPDATE tasks SET timer = $1 WHERE id = $2`, [timer, id], (err, result) => {
         if (err) {
             console.log(err);
-            res.status(500).send('Error updating task');
+            res.status(500).send('Erro interno do servidor.');
         }
-        res.status(200).send('Timer Task updated');
+        res.status(200).send('Timer da tarefa atualizado.');
     });
 });
 
@@ -113,22 +123,22 @@ app.delete('/data', (req, res) => {
     const { id } = req.query;
 
     if (!id) {
-        res.status(400).send('Invalid id');
+        res.status(400).send('ID inválido.');
         return;
     }
 
     client.query(`DELETE FROM tasks WHERE id = $1`, [id], (err, result) => {
         if (err) {
-            res.status(500).send('Error finding task');
+            res.status(500).send('Erro interno do servidor.');
             return;
         }
 
         if (result.rowCount === 0) {
-            res.status(404).send('Task not found');
+            res.status(404).send('Tarefa não encontrada.');
             return;
         }
 
-        res.status(200).send('Task deleted');
+        res.status(200).send('Tarefa excluída com sucesso.');
     });
 
 });
